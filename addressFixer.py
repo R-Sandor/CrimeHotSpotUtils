fa9460c8b9c6fd27f8e718ad23672248e365934c
rsandor <rsand006@odu.edu>
2019-02-02 10:10:02 -0500

    added addressFixer which will return csv of cleaned address and lat, long

--- addressFixer.py
+++ addressFixer.py
@@ -1,16 +1,24 @@
 import csv
+import requests
 #Raphael J. Sandor
 #1/27/19
 #CS41W
-#Fixes the address in a CSV so they may be geocoded.
 
-from geopy.geocoders import Nominatim
-geolocator = Nominatim(user_agent="Crime HotSpot")
-location = geolocator.geocode("1000 W 41st ST, Norfolk")
-print(location)
 
-infile = open('2017Crimemapping.csv', 'r+')
+GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?&key=AIzaSyAUMJIcUHZdtUOB0TtFzCY13Mc0x33yyy0'
+params = {
+'address': ' ',
+'sensor': 'false',
+'region': 'USA'
+}
+
+print (params)
+
+outFile = open('addressRow.csv', 'w')
+infile = open('2017Crimemapping.csv', 'r')
 csv_reader = csv.reader(infile, delimiter=',')
+address_writer = csv.writer(outFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
 line_count = 0 
 for row in csv_reader:
     if line_count > 1:
@@ -18,9 +26,23 @@ for row in csv_reader:
         line_count+=1
         row[4]=(row[4].replace("   -", ","))
         address = (row[4].replace("<>", "").replace("BLK", "")) 
-        print(address)
-                
+        print("Before going to google:  "+ address)
+        print("----------------------------------")
+        params['address']=address
+        req = requests.get(GOOGLE_MAPS_API_URL, params=params)
+        res = req.json()
+       # print(res)
+        result = res['results'][0]
+        print(result)
+        geodata = dict()
+        geodata['lat'] = result['geometry']['location']['lat']
+        geodata['lng'] = result['geometry']['location']['lng']
+        geodata['address'] = result['formatted_address']
+        print('{address}. (lat, lng) = ({lat}, {lng})'.format(**geodata))
+        address_writer.writerow([geodata['address'], geodata['lat'], geodata['lng']])
+        
     else:    
         line_count+=1
 print(line_count)    
 infile.close
+outFile.close
